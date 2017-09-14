ae846ba3e382c9de598fefeb36ee2542ddf33cf3
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-09-14 06:53:24 +0300

    * Init views and binding the keyboard to the textview

--- app/src/main/java/com/yonikal/sample/MainActivity.java
+++ app/src/main/java/com/yonikal/sample/MainActivity.java
@@ -2,15 +2,25 @@ package com.yonikal.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.yonikal.R;
+import com.yonikal.keyboardview.KeyboardView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    // Views
+    KeyboardView keyboardView;
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview = (TextView) findViewById(R.id.textview);
+        keyboardView = (KeyboardView) findViewById(R.id.keyboard);
+        keyboardView.bindTo(textview);
     }
 }
